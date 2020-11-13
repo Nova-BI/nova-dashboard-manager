8e1470799a666442af68ac1c9726f0c143fad802
Cord <cf@lct.de>
2020-11-13 12:39:46 +0100

    Update BaseFilter.php
    fix search issue

--- src/Nova/Datafilterables/BaseFilter.php
+++ src/Nova/Datafilterables/BaseFilter.php
@@ -44,9 +44,21 @@ class BaseFilter extends Resource
      *
      * @var  array
      */
-    public static $search = [
-        'name'
-    ];
+    public static $search = [];
+    
+        /**
+     * Indicates if the resource should be globally searchable.
+     *
+     * @var bool
+     */
+    public static $globallySearchable = false;    
+    
+    /**
+     * Indicates if the resource should be searchable on the index view.
+     *
+     * @var bool
+     */
+    public static $searchable = false;    
 
     /**
      * Get the fields displayed by the resource.
