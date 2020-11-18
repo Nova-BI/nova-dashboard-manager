ff816dd0bb5288b364e92a415d4eea5037a0541d
Maris <webstarty@gmail.com>
2020-11-18 17:28:31 +0100

    added default dashboard type to prevent error

--- src/Nova/DashboardConfiguration.php
+++ src/Nova/DashboardConfiguration.php
@@ -110,7 +110,7 @@ class DashboardConfiguration extends Resource
         $fields = [
             InlineMorphTo::make(__('Board Type'), 'dashboardable')
                 ->types($dashboardables)->required()->hideFromIndex()
-//                ->default(),
+                ->default(\NovaBi\NovaDashboardManager\Nova\Dashboardables\Standard::class),
         ];
 
         return
